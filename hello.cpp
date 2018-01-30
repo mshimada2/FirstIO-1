67453cd32275302462ec20da2a979d7f384a6ca6
mshimada <mshimada2@mail.csuchico.edu>
2018-01-29 17:14:50 -0800

    final version

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,5 @@
+//Author: McDermott Liam Shimada  
+
 #include <iostream>
 #include <string>
 
@@ -5,6 +7,6 @@ int main() {
     std::string input;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hi my name is, " << input <<"!"<< std::endl;
     return 0;
 }
